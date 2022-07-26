b80bb3e1bdb5cbff3f0d19c2b4012d51f3252156
Rene Huber <huberskull@gmail.com>
2022-07-26 13:22:57 +0200

    save

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', require('./routes/goal.Routes'));
 app.use('/api/users', require('./routes/user.Routes'));
 
-// Serve frontend 
+// Serve client 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
     )
   );
 } else {
--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI)
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`.trap.bold.green)
+    console.log(`MongoDB Connected: ${conn.connection.host}`.trap.bold.green.bgBlack)
   } catch (error) {
     console.log(error)
     process.exit(1)
