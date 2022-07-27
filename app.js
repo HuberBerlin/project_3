6783c7f762a4996c9586c7a194c7d40a69d595d7
Rene Huber <huberskull@gmail.com>
2022-07-27 17:23:20 +0200

    some awasome ccs

--- app.js
+++ app.js
@@ -23,12 +23,16 @@ app.use('/api/goals', require('./routes/goal.Routes'));
 app.use('/api/users', require('./routes/user.Routes'));
 
 // Serve client 
+
+
+
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      path.resolve(__dirname, './', 'client', 'build', 'index.html')
     )
   );
 } else {
--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 // Serve client
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, './client/build')));
 
   app.get('*', (req, res) =>
     res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      path.resolve(__dirname, './', 'client', 'build', 'index.html')
     )
   );
 } else {
